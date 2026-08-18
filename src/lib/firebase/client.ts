2cebf6b7f6d173a877eeb675ee82f7164d296d41
ZxDelt <j.bodse98@gmail.com>
2026-08-18 17:21:18 +0530

    Configure Firebase project: treasure-hunt-49dbb

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -9,12 +9,12 @@ import {
 import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCyberHuntDevKeyDemoICAT2026",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "icat-treasurehunt-2026.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "icat-treasurehunt-2026",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "icat-treasurehunt-2026.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "102938475610",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:102938475610:web:abcdef1234567890",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAHMboFhM1bl5V8Igdq_oZC2mfUXZ6zfr0",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "treasure-hunt-49dbb.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "treasure-hunt-49dbb",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "treasure-hunt-49dbb.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "14450654778",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:14450654778:web:bd663ca790bcf28ba17f03",
 };
 
 let app: FirebaseApp;
